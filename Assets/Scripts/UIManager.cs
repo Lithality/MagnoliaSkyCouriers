c2cbac0b40fe097481658eb6484e6209416837fb
longarch <wesleyccj@yahoo.com.sg>
2016-01-04 23:08:36 +0900

    update

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,27 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
-using DG.Tweening;
-using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
-	[SerializeField]
-	Button btn;
-
 	// Use this for initialization
 	void Start () {
-
-		btn.gameObject.transform.DOPunchRotation (new Vector3 (0, 0, 90), 3.0f, 10, 1).SetUpdate (true);
-
-
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-
-
 	}
 
 	public void loadLevel(string s)
